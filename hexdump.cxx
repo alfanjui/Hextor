1c43a3750a7218bec7c4571e3efedfc281bc041e
アルファンヒー <62527857+alfanjui@users.noreply.github.com>
2022-05-25 18:52:59 +0000

    implemented a bitset buffer
    Bitset buffer to cast content properly in its binary form

--- hexdump.cxx
+++ hexdump.cxx
@@ -10,44 +10,22 @@ using std::string; using std::hex;
 using std::stringstream; using std::endl;
 using std::bitset;
 
-int ascii_hexdump()
-{
-	std::ifstream file;
-	string filename;
-	
-	cout << "Enter the file's name > "; cin >> filename;
-	file.open(filename, std::ios::in);
-	if (file.is_open()) {
-		char chara[3];
-		while (!file.eof()) {
-			for (int i = 0; sizeof(chara); i++) {
-				chara[i] = file.get();
-				printf("%0X", chara[i]);
-			}
-			cout << " ";
-			//printf(chara);
-		}
-	}
-	else {
-		cout << "File couldn't be open or doesn't exist\n";
-	}
-	return EXIT_SUCCESS;
-}
-
 int binary_hexdump()
 {
 	std::ifstream file;
 	string filename;
+	string binRep;
 
 	cout << "Enter the file's name > "; cin >> filename;
 	file.open(filename, std::ios::in);
 	if (file.is_open()) {
 		char chara[3];
+		char tmpRep;
 		while (!file.eof()) {
 			for (int i = 0; i < 4; i++) {
 				chara[i] = file.get();
-				bitset<8> bsh(chara[i]); 
-				printf("%0X", bsh[i]);
+				bitset<8>(chara[i]).to_string(); 
+				printf("%0X", chara[i]);
 			}
 			cout << " ";
 			//printf(chara);
@@ -65,15 +43,10 @@ int main()
 
 	while (true)
 	{
-		cout << "(0) ASCII Hexadecimal Dump" <<	"\n"
 		 	 << "(1) Binary Hexadecimal Dump" << "\n"
 		 	 << "Choose mode > "; cin >> mode;
 		
 		switch (mode) {
-			case 0:
-				ascii_hexdump();
-				break;
-
 			case 1:
 				binary_hexdump();
 				break;
